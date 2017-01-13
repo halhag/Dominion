79783b285b94655d9fdd7ddbcdb12a7b70552afe
Halvard Hagesether <halhag@knowit.no>
2017-01-13 18:31:25 +0100

    Better date in rating list.

--- Dominion/Controllers/RatingListController.cs
+++ Dominion/Controllers/RatingListController.cs
@@ -28,7 +28,7 @@ namespace Dominion.Controllers
                 {
                     Name = sortedResult.Player,
                     Rating = Math.Round(sortedResult.Number, 1),
-                    LastPlayed = sortedResult.LastPlayed.ToShortDateString(),
+                    LastPlayed = GetLastPlayed(sortedResult.LastPlayed),
                     Wins = sortedResult.NumberOfWonGames,
                     Draws = sortedResult.NumberOfDrawnGames,
                     Losses = sortedResult.NumberOfLostGames
@@ -37,6 +37,58 @@ namespace Dominion.Controllers
             }
 
             return View(ratingListModel);
+        }
+
+        private string GetLastPlayed(DateTime dateTime)
+        {
+            DateTime today = DateTime.Today;
+            var differenceInDays = (today - dateTime).Days;
+
+            if (differenceInDays == 0)
+            {
+                return "Today";
+            }
+            if (differenceInDays == 1)
+            {
+                return "Yesterday";
+            }
+            if (differenceInDays > 1 && differenceInDays < 7)
+            {
+                return differenceInDays + " days ago";
+            }
+            if (differenceInDays > 6 && differenceInDays < 14)
+            {
+                return "A week ago";
+            }
+            if (differenceInDays > 13 && differenceInDays < 21)
+            {
+                return "Two weeks ago";
+            }
+            if (differenceInDays > 20 && differenceInDays < 30)
+            {
+                return "Some weeks ago";
+            }
+            if (differenceInDays > 29 && differenceInDays < 60)
+            {
+                return "A month ago";
+            }
+            if (differenceInDays > 59 && differenceInDays < 90)
+            {
+                return "Two months ago";
+            }
+            if (differenceInDays > 89 && differenceInDays < 365)
+            {
+                return "Months ago";
+            }
+            if (differenceInDays > 364 && differenceInDays < 730)
+            {
+                return "A year ago";
+            }
+            if (differenceInDays > 729)
+            {
+                return "Forever ago";
+            }
+            return "Huh?";
         }
     }
 }
