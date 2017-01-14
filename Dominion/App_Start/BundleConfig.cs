3db44c90fc1f1069e26607b216cc0ca10a8f87ff
Halvard <halvardhagesaether@gmail.com>
2017-01-14 23:06:11 +0100

    Styling lagt til, ala IKWYT.

--- Dominion/App_Start/BundleConfig.cs
+++ Dominion/App_Start/BundleConfig.cs
@@ -24,6 +24,35 @@ namespace Dominion
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/assets/css/layoutnewstyle").Include(
+                    "~/assets/css/assets.css",
+                    "~/assets/css/style.css"
+                    ));
+
+            bundles.Add(new StyleBundle("~/assets/css/layoutnewrevolutionstyle").Include(
+                "~/assets/revolution/fonts/pe-icon-7-stroke/css/pe-icon-7-stroke.css",
+                "~/assets/revolution/css/settings.css",
+                "~/assets/revolution/css/layers.css",
+                "~/assets/revolution/css/navigation.css",
+                "~/assets/css/skins/default.css"
+                ));
+
+            bundles.Add(new ScriptBundle("~/bundles/layoutnewscripts").Include(
+                    "~/assets/js/assets.min.js",
+                    "~/assets/revolution/js/jquery.themepunch.revolution.min.js",
+                    "~/assets/revolution/js/jquery.themepunch.tools.min.js"
+                    ));
+
+            bundles.Add(new ScriptBundle("~/bundles/layoutnewmainscripts").Include(
+                   "~/assets/js/script.js"
+                   ));
+
+            bundles.Add(new StyleBundle("~/assets/css/sitelayoutsstyle").Include(
+                "~/Scripts/bower_components/chartist/dist/chartist.min.css", "~/Scripts/jquery-ui-1.11.4.custom/jquery-ui.min.css",
+                "~/assets/css/assets.css", "~/assets/css/style.css", "~/assets/css/light.css"
+                //,"~/assets/css/skins/default.css"
+                ));
         }
     }
 }
